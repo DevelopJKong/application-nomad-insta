9a00e52ac55b1c67cdbc5c9ba2968d5be7d7c071
cafe small house <tbaram10@naver.com>
2023-04-27 22:51:19 +0900

    eslint 설정 수정

--- src/pages/logout/create-account.page.tsx
+++ src/pages/logout/create-account.page.tsx
@@ -1,43 +1,21 @@
 import React from 'react';
-import { TextInput } from 'react-native';
 import AuthLayout from '../../components/auth/auth-layout.component';
 import AuthButton from '../../components/auth/auth-button.component';
+import styled from 'styled-components/native';
+
+const Input = styled.TextInput`
+   background-color: white;
+   width: 100%;
+`;
 
 const CreateAccount = () => {
    return (
       <AuthLayout>
-         <TextInput
-            placeholder='First Name'
-            placeholderTextColor='gray'
-            returnKeyType='next'
-            style={{ backgroundColor: 'white', width: '100%' }}
-         />
-         <TextInput
-            placeholder='Last Name'
-            placeholderTextColor='gray'
-            returnKeyType='next'
-            style={{ backgroundColor: 'white', width: '100%' }}
-         />
-         <TextInput
-            placeholder='Username'
-            placeholderTextColor='gray'
-            returnKeyType='next'
-            style={{ backgroundColor: 'white', width: '100%' }}
-         />
-         <TextInput
-            placeholder='Email'
-            placeholderTextColor='gray'
-            keyboardType='email-address'
-            returnKeyType='next'
-            style={{ backgroundColor: 'white', width: '100%' }}
-         />
-         <TextInput
-            placeholder='Password'
-            placeholderTextColor='gray'
-            secureTextEntry
-            returnKeyType='done'
-            style={{ backgroundColor: 'white', width: '100%' }}
-         />
+         <Input placeholder='First Name' placeholderTextColor='gray' returnKeyType='next' />
+         <Input placeholder='Last Name' placeholderTextColor='gray' returnKeyType='next' />
+         <Input placeholder='Username' placeholderTextColor='gray' returnKeyType='next' />
+         <Input placeholder='Email' placeholderTextColor='gray' keyboardType='email-address' returnKeyType='next' />
+         <Input placeholder='Password' placeholderTextColor='gray' secureTextEntry returnKeyType='done' />
          <AuthButton text={'Create Account'} disabled={true} onPress={() => null} />
       </AuthLayout>
    );
