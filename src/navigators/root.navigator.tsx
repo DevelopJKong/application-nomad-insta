172149cf9a084108099f7d9fd631ab9953a34f0c
cafe small house <tbaram10@naver.com>
2023-04-27 22:42:04 +0900

    root.navigator 수정

--- src/navigators/root.navigator.tsx
+++ src/navigators/root.navigator.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Stacks from './stack.navigator';
 
 const Nav = createNativeStackNavigator();
 const Root = () => {
    return (
       <Nav.Navigator>
-         <Nav.Screen />
+         <Nav.Screen key={'Stack'} name={'Stack'} component={Stacks} />
       </Nav.Navigator>
    );
 };
