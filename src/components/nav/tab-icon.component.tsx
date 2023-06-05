627b19305b38ff80335a21da8de5a864b1ef020c
cafe small house <tbaram10@naver.com>
2023-06-05 09:04:06 +0900

    자동완성도 가능하도록 타입 변경

--- src/components/nav/tab-icon.component.tsx
+++ src/components/nav/tab-icon.component.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
 
 interface ITabIcon {
-   iconName: 'home' | 'search' | 'camera' | 'heart' | 'person';
+   iconName: keyof typeof Ionicons.glyphMap | undefined;
    color: string;
    focused: boolean;
 }
 
 const TabIcon = ({ iconName, color, focused }: ITabIcon) => {
-   return <Ionicons name={focused ? iconName : `${iconName}-outline`} color={color} size={focused ? 22 : 20} />;
+   return (
+      <Ionicons
+         name={focused ? iconName : (`${iconName}-outline` as keyof typeof Ionicons.glyphMap)}
+         color={color}
+         size={focused ? 22 : 20}
+      />
+   );
 };
 
 export default TabIcon;
