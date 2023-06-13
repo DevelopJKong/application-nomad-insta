66e70e4b6ce5fcdc395d393ed74223e920d20615
cafe small house <tbaram10@naver.com>
2023-06-13 09:02:35 +0900

    #16.7 Photo part Two 완료

--- src/components/photo/photo.component.tsx
+++ src/components/photo/photo.component.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
-import { useWindowDimensions } from 'react-native';
+import { useWindowDimensions, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 interface IPhotoComponent {
    id: number;
@@ -15,13 +17,21 @@ interface IPhotoComponent {
    commentNumber: number;
 }
 
-const Container = styled.View`
-   background-color: red;
+const Container = styled.View``;
+const Header = styled.TouchableOpacity`
+   padding: 10px;
+   flex-direction: row;
+   align-items: center;
 `;
-const Header = styled.View``;
-const UserAvatar = styled.Image``;
+const UserAvatar = styled.Image`
+   margin-right: 10px;
+   width: 25px;
+   height: 25px;
+   border-radius: 12.5px;
+`;
 const Username = styled.Text`
    color: white;
+   font-weight: 600;
 `;
 const File = styled.Image``;
 const Actions = styled.View``;
@@ -33,16 +43,37 @@ const CaptionText = styled.Text`
 const Likes = styled.Text`
    color: white;
 `;
+const UsernameBtn = styled.TouchableOpacity``;
+
+export type NavigationType = NativeStackNavigationProp<any>;
 
 const PhotoComponent = ({ id, user, caption, file, isLiked, likes, commentNumber }: IPhotoComponent) => {
    const { width, height } = useWindowDimensions();
+   const [imageHeight, setImageHeight] = useState<number>(height - 450);
+   const navigation = useNavigation<NavigationType>();
+
+   useEffect(() => {
+      Image.getSize(file, (width, height) => {
+         console.log(imageHeight);
+         setImageHeight(height / 3);
+      });
+   }, [file]);
    return (
       <Container>
-         <Header>
-            {/* <UserAvatar />*/}
+         <Header onPress={() => navigation.navigate('Profile')}>
+            <UserAvatar
+               resizeMode={'cover'}
+               source={{ uri: user.avatar }}
+               style={{
+                  width: 50,
+                  height: 50,
+                  borderRadius: 25,
+               }}
+            />
             <Username>{user.username}</Username>
          </Header>
          <File
+            resizeMode={'contain'}
             style={{
                width,
                height: height - 200,
@@ -55,7 +86,9 @@ const PhotoComponent = ({ id, user, caption, file, isLiked, likes, commentNumber
          </Actions>
          <Likes>{likes === 1 ? '1 like' : `${likes} likes`}</Likes>
          <Caption>
-            <Username>{user.username}</Username>
+            <UsernameBtn onPress={() => navigation.navigate('Profile')}>
+               <Username>{user.username}</Username>
+            </UsernameBtn>
             <CaptionText></CaptionText>
          </Caption>
       </Container>
